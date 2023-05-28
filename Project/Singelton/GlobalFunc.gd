ec70ed1620a4b2d29f33f90605532dd502c26183
AftDawn <declanjeffrey6@gmail.com>
2023-05-28 14:26:54 +1000

    Made Global Files with the beginning of things to come Added Godot plugin "GodotSteam" and "godot-git-plugin" to gitignore

---
+++ Project/Singelton/GlobalFunc.gd
@@ -0,0 +1,14 @@
+extends Node
+class_name GlobalFunc
+
+const save_file_path = "user://Save"
+const save_file_name = "Save"
+const save_file_extension = ".ascgsave"
+var save_file_selected = 1
+
+const option_file_path = "user://"
+const option_file_name = "Options"
+const option_file_extension = ".ascgopts"
+
+func save_options():
+	pass
